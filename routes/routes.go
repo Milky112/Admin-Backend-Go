3950edb146d98b52593c377d60136ec022c083c9
Deni <juztdeni@gmail.com>
2022-04-07 01:08:33 +0700

    Go Backend Admin Register/Login
    Create Register Admin login using JWT and Mysql

---
+++ routes/routes.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"fibr/controllers"
+	"fibr/middleware"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Setup(app *fiber.App) {
+	app.Post("/api/register", controllers.Register)
+	app.Post("/api/login", controllers.Login)
+
+	app.Use(middleware.IsAuthenticated)
+
+	app.Get("/api/user", controllers.User)
+	app.Post("/api/logout", controllers.Logout)
+
+	app.Get("/api/users", controllers.AllUsers)
+	app.Post("/api/users", controllers.CreateUser)
+}
